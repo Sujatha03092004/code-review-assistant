f1c2c5a5ed98dec221bb2f4ccd9a365280d047e4
unknown <sujathagd394@gmail.com>
2026-03-31 00:40:23 +0530

    Fixed fetching of backend issue

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI(
     version = "1.0.0"
 )
 
-app.add_middleware(CORSMiddleware, allow_origins = ["http://localhost:3000", "https://code-review-assistant-dun.vercel.app/"], allow_credentials = True, allow_methods = ["*"], allow_headers = ["*"], )
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials = True, allow_methods = ["*"], allow_headers = ["*"], )
 
 app.include_router(review_router, prefix = "/api")
 
